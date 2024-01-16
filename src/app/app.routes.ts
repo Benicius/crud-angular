b197d241adcfb8b96d15797a8d5c643eda814209
Benicius <beniciusclaro@hotmail.com>
2024-01-16 22:07:19 +0000

    Criando o projeto Angular inicial, e adicionando um modulo de Courses junto com o Routing e modulo que é importante, não esqueça

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CoursesComponent } from './courses/courses/courses.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(course => course.CoursesModule) }
+];
