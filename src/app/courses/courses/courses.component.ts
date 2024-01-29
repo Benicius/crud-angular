0ab97013c036bda3cf05892c0f911d40d691905a
Benicius <beniciusclaro@hotmail.com>
2024-01-29 23:19:16 +0000

    Creating material theme, sever side, and generics to import

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
+
 export class CoursesComponent {
+
+  courses: Observable<Course[]>;
+  //courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
+
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+    //this.courses = [];
+    //this.coursesService = new CoursesService();
+    //this.coursesService.list().subscribe(courses => this.courses = courses);
+    this.courses = this.coursesService.list();
+  }
 
 }
